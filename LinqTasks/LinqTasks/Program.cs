2b27eabaa4f177739fb4cc5283eaf34ed74a315e
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-10-14 07:43:15 +0200

    no message

--- LinqTasks/LinqTasks/Program.cs
+++ LinqTasks/LinqTasks/Program.cs
@@ -6,6 +6,8 @@ namespace LinqTasks
 {
     internal class Program
     {
+
+        
         static void Main(string[] args)
         {
             //int[] list = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
